39d7c59a5e2cfdb51c9feeb32971dd1bfea1c6d7
Ezekiel Halley <eze.halley@gmail.com>
2022-09-09 01:13:31 -0400

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,73 @@
+use std::vec;
+
+const ASCII_A: u8 = 'A' as u8;
+const ALPHABET_SIZE: u8 = 26;
+
+fn main() {
+    //println!("{}", alphabet_index_to_string(vec![13, 20, 12, 1, 4, 17, 19, 7, 4, 14, 17, 24]));
+    //let v = string_to_alphabet_index(String::from("ljlljltjunjuuj"));
+    let msg = String::from("BON");
+    let msg_indicies = string_to_alphabet_index(&msg);
+    let e_indicies = encrypt_affine((4, 13), &msg);
+    let e_msg = alphabet_index_to_string(&e_indicies);
+    
+    println!("{msg}");
+    println!("{msg_indicies:?}");
+    println!("{e_msg}");
+    println!("{e_indicies:?}");
+}
+
+
+fn string_to_alphabet_index(text: &String) -> Vec<u8> {
+    let mut index_array: Vec<u8>= Vec::new();
+
+    for c in text.to_uppercase().chars(){
+        let text_index = (c as u8) - ASCII_A; 
+        index_array.push(text_index);
+    }
+    index_array
+}
+
+
+fn alphabet_index_to_string(a_indicies: &Vec<u8>) -> String{
+    let mut letters = String::new();
+    for value in a_indicies{
+        letters.push((value + ASCII_A) as char)
+    }
+    letters
+}
+
+fn gcd(mut a: u8, mut b: u8) -> u8 {
+    while b != 0 {
+        let r = a % b;
+        a = b;
+        b = r;
+    }
+    a
+}
+
+fn mod_inv(a: u8, n: u8) -> u8 {
+    if gcd(a, n) == 1 {
+
+    }
+    0
+}
+
+fn encrypt_affine(key: (u32, u32), msg: &String) -> Vec<u8> {
+    let mut encrypted_msg: Vec<u8> = Vec::new();
+    let alphabet_index_arr = string_to_alphabet_index(msg);
+
+    for alpha_i in alphabet_index_arr{
+        let alpha_i = alpha_i as u32;
+        let e_val = (alpha_i * key.0 + key.1) % ALPHABET_SIZE as u32;
+        encrypted_msg.push(e_val as u8);
+    }
+    encrypted_msg
+}
+
+/*
+fn decrypt_affine(key: (u32, u32), msg: String) -> String{
+    let mut decrypted_msg = String::new();
+    decrypted_msg
+}
+*/
